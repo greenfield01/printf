2207bdd1d5a25b97299461cfd11857a80cf0c047
Greenfield Adolphus <greenfieldadolphus22@gmail.com>
2022-10-17 18:23:57 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <limits.h>
 
 char *_strrev (char *str)
 {
@@ -47,19 +48,20 @@ char * _itoa(int i, char *strout, int base)
   return strout;
 }
 
-int print (char * str, ...)
+// int _printf (char * str, ...)
+int _printf(const char *format, ...)
 {
   va_list vl;
   int i = 0, j=0;
   char buff[100]={0}, tmp[20];
-  char * str_arg;
+  char * format_arg;
   
-  va_start( vl, str );
-  while (str && str[i])
+  va_start( vl, format );
+  while (format && format[i])
   {
-    if(str[i] == '%'){
+    if(format[i] == '%'){
       i++;
-      switch (str[i]) {
+      switch (format[i]) {
         /* Convert char */
         case 'c': {
           buff[j] = (char)va_arg( vl, int );
@@ -73,6 +75,15 @@ int print (char * str, ...)
           j += strlen(tmp);
           break;
         }
+        /* Convert integer */
+        // case 'i': {
+        // //   atoi(va_arg( vl, int ));
+        //   int k = atoi(va_arg( vl, int ))
+        //   strcpy(&buff[j], k);
+        //   j += strlen(k);
+        //   break;
+        // }
+
         /* Convert hex */
         case 'x': {
           _itoa(va_arg( vl, int ), tmp, 16);
@@ -89,14 +100,14 @@ int print (char * str, ...)
         }
         /* copy string */
         case 's': {
-          str_arg = va_arg( vl, char* );
-          strcpy(&buff[j], str_arg);
-          j += strlen(str_arg);
+          format_arg = va_arg( vl, char* );
+          strcpy(&buff[j], format_arg);
+          j += strlen(format_arg);
           break;
         }
       }
     } else {
-      buff[j] =str[i];
+      buff[j] =format[i];
       j++;
     }
     i++;
@@ -104,4 +115,47 @@ int print (char * str, ...)
   fwrite(buff, j, 1, stdout); 
   va_end(vl);
   return j;
+}
+// int main (int argc, char *argv[])
+// {
+//   int ret;
+//   ret = print("%c %d %o %x %s\n", 'A', 10, 100, 1000, "Hello from printf!");
+//   printf("printf returns %d bytes\n", ret);
+//   return 0;
+// }
+
+int main(void)
+{
+    int len;
+    int len2;
+    unsigned int ui;
+    void *addr;
+
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    ui = (unsigned int)INT_MAX + 1024;
+    addr = (void *)0x7ffe637541f0;
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+    _printf("Unsigned:[%u]\n", ui);
+    printf("Unsigned:[%u]\n", ui);
+    _printf("Unsigned octal:[%o]\n", ui);
+    printf("Unsigned octal:[%o]\n", ui);
+    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    _printf("Address:[%p]\n", addr);
+    printf("Address:[%p]\n", addr);
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
+    _printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
+    _printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");
+    return (0);
 }
