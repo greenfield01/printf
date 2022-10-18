749a0416b9f8c77b4b72b6fc071f3cb465a4a75a
ufaz <talk2ufaz@gmail.com>
2022-10-18 16:29:11 -0700

    prinft

--- get_width.c
+++ get_width.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * get_width - Calculates the width for printing
- * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
+ * get_width - Calcs width for printing
+ * @format: Formatted string to print the args.
+ * @i: List of args to print.
+ * @list: list of args.
  * Return: width.
  */
 int get_width(const char *format, int *i, va_list list)
